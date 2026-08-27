c17e6695106d8131122aed5b5a2c593267273f6c
Zac Lou <97340247+ZacLou@users.noreply.github.com>
2026-08-28 00:49:28 +0800

    feat: add skeleton loaders barrel export
    Part of #3

---
+++ components/app/skeletons/index.ts
@@ -0,0 +1,3 @@
+export { default as MarketSkeleton } from "./MarketSkeleton"
+export { default as InvoiceDetailSkeleton } from "./InvoiceDetailSkeleton"
+export { default as ClaimStatusSkeleton } from "./ClaimStatusSkeleton"
